51abba243575c077259ae8377ff30d4bf0807443
oceanide <oceanide@gmail.com>
2020-04-23 09:03:56 +0900

    add ClickCounter-spec-1
    ClickCounter모듈의 getValue()는 카운트 값을 반환한다.

---
+++ test/ClickCounter.spec.js
@@ -0,0 +1,9 @@
+describe('App.ClickCounter', () => {
+  jasmine.getEnv().configure({ random: false });
+  describe('getValue()', () => {
+    it('초기값이 0인 카운터 값을 반환한다', () => {
+      const counter = App.ClickCounter();
+      expect(counter.getValue()).toBe(0);
+    });
+  });
+});
